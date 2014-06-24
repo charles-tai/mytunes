f03b84422bfaebbe6169004a9577073ac79e963f
Charles Tai <cwtai86@gmail.com>
2014-06-24 13:29:04 -0700

    Clean up notes

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -1,33 +1,19 @@
 // App.js - Defines a backbone model class for the whole app.
 var AppModel = Backbone.Model.extend({
 
-  //pass a library object
   initialize: function(params){
     this.set('currentSong', new SongModel());
     this.set('songQueue', new SongQueue());
     var songQueue = this.get('songQueue');
 
-    /* Note that 'this' is passed as the third argument. That third argument is
-    the context. The 'play' handler will always be bound to that context we pass in.
-    In this example, we're binding it to the App. This is helpful because otherwise
-    the 'this' we use that's actually in the funciton (this.set('currentSong', song)) would
-    end up refering to the window. That's just what happens with all JS events. The handlers end up
-    getting called from the window (unless we override it, as we do here). */
-
-    // library play event handler not needed by songQueue handles play functionality
-    // params.library.on('play', function(song){
-    //   this.set('currentSong', song);
-    // }, this);
-
-    //library event handlers
-    // context not needed because songQueue defined as variable
+    // library event handlers
     params.library.on('enqueue', function(song) {
       songQueue.add(song);
       console.log('library enqueue, model"s collection');
       console.log(songQueue);
     });
 
-    //songQueue event handlers
+    // songQueue event handlers
     songQueue.on('play', function(song){
       this.set('currentSong', song);
     },this);
